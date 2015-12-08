235f670828ee246ca36e7f222a61c3572aff46f7
YSenarath <wayasas@gmail.com>
2015-12-08 19:58:22 +0530

    Initial Commit: Contains symfony base.

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on December 8, 2015, 1:23 pm.
---
+++ src/EmsBundle/EmsBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace EmsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EmsBundle extends Bundle
+{
+
+}
