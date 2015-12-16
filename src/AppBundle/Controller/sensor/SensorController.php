b2e200ee3e8ae5d57d6f95dcdb2c75d41c62734e
Nadheesh <nadheeshj@gmail.com>
2015-12-16 20:39:10 +0530

    senosr list under construction

--- src/AppBundle/Controller/sensor/SensorController.php
+++ src/AppBundle/Controller/sensor/SensorController.php
@@ -9,26 +9,42 @@
 namespace AppBundle\Controller\sensor;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\sensor\Sensor;
+use Doctrine\DBAL\Connection;
+
 
 class SensorController extends  Controller{
 
     private $connection;
 
-    public function __construct($connection)
+    public function __construct(Connection $connection)
     {
         $this->connection = $connection;
     }
 
-
-    public function sensorListAction($username)
+    public function getAllSensors()
     {
-        $sensor = $this->connection->fetchAssoc('SELECT * FROM sensor WHERE user_name = ?', array($username));
+        $result = $this->connection->executeQuery('SELECT * FROM sensor ORDER BY sensor_id');
+        $result = $result->fetchAll();
 
-        if ($sensor != null)
-            return $sensor;
-        return false;
+        //print_r($result);
+        $sensors[] = new Sensor();
+
+        foreach ($result as $s) {
+            if ($s != null) {
+                $sensor = new Sensor();
+                $sensor->setSensorId($s["sensor_id"]);
+                $sensor->setTypeId($s["type_id"]);
+                $sensor->setModelId($s["model_id"]);
+                $sensor->setInsDate($s["installed_date"]);
+                $sensor->setTMin($s["threshold_min"]);
+                $sensor->setTMax($s["threshold_max"]);
+                $sensor->setLocId($s["location_id"]);
+                $sensors[] = $sensor;
+            }
+        }
+
+        return $sensors;
     }
 
 
