b41908dfd934fe186740acfeae00a589f377cb79
mithwick93 <quadro.ny2s@gmail.com>
2015-12-17 00:24:22 +0530

    Added tab icon as a local resource

--- src/AppBundle/Controller/sensor/SensorController.php
+++ src/AppBundle/Controller/sensor/SensorController.php
@@ -8,10 +8,9 @@
  */
 namespace AppBundle\Controller\sensor;
 
-use Doctrine\DBAL\Connection;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\sensor\Sensor;
 use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class SensorController extends Controller
@@ -58,16 +57,20 @@ class SensorController extends Controller
     public function getSensorIdsByLocationAction($locationId)
     {
 
-        $result = $this->connection->executeQuery('SELECT sensor_id,type_id FROM sensor WHERE location_id=? ORDER BY sensor_id',array($locationId));
+        $result = $this->connection->executeQuery(
+            'SELECT sensor_id,type_id FROM sensor WHERE location_id=? ORDER BY sensor_id',
+            array($locationId)
+        );
         $result = $result->fetchAll();
         //print_r($result);
         $sensorIdArray = array();
 
         foreach ($result as $a) {
             if ($a != null) {
-                $sensorIdArray[] = array($a["sensor_id"],$a["type_id"]);
+                $sensorIdArray[] = array($a["sensor_id"], $a["type_id"]);
             }
         }
+
         //print_r($sensorIdArray);
 
         return $sensorIdArray;
