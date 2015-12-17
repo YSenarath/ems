151bde323d52bf8a79c39a889940e00f2066084a
YSenarath <wayasas@gmail.com>
2015-12-17 08:56:22 +0530

    Dulanjaya: Location displays on the google map, location adding forum added

---
+++ src/AppBundle/Controller/location/LocationController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yasas
+ * Date: 12/11/2015
+ * Time: 6:26 PM
+ */
+namespace AppBundle\Controller\location;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LocationController extends Controller
+{
+    private $connection;
+
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getLocationsAction()
+    {
+        $locations = $this->connection->fetchAll('SELECT location_id, longitude, latitude FROM location');
+
+        if ($locations != null)
+            return $locations;
+        return false;
+    }
+}
