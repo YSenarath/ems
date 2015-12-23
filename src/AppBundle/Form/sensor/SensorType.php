ae49dbadff5b7bf9b42903eeb7f1f03ccee0d6ae
Nadheesh <nadheeshj@gmail.com>
2015-12-23 09:18:21 +0530

    Merge branch 'master' of https://bitbucket.org/quadro-2/ems
    Conflicts:
    	.idea/workspace.xml
    	src/AppBundle/Form/sensor/SensorType.php

--- src/AppBundle/Form/sensor/SensorType.php
+++ src/AppBundle/Form/sensor/SensorType.php
@@ -16,7 +16,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 use AppBundle\Entity\sensor\Model;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
